b1ad39eabe418df3cb2e6dec2339fdd5cfbd5cc9
thiraphong <terapong4100@gmail.com>
2026-05-14 12:12:44 +0700

    ๊Update Vite cinfig

--- quiz-system/vite.config.js
+++ quiz-system/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz/',
+  base: '/bms_training/quiz/',
   server: { port: 5173, base: '/quiz/' },
   build: {
     outDir: 'dist',
